2df33a728973541f8b94de1d77368e430a59cf30
Keo <kawan.keo@mtsc.co.th>
2023-09-19 23:34:36 +0700

    Add systemd, socket for tracking

--- time_tracking/main_file.py
+++ time_tracking/main_file.py
@@ -1,12 +1,83 @@
 import time
+import socket
+import argparse
+
+
+localhost_network = "127.0.0.1"
+socket_port = 52000
+
+
+# Sender Data to socket
+def send_data():
+    try:
+        # Create a socket object
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Connect to Receiver
+        s.connect((localhost_network, socket_port))
+
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+
+        s.send(current_time.encode())
+
+        # Close the connection
+        s.close()
+    except Exception:
+        pass
+
+
+# Receive Data from socket
+def receive_data():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Set the SO_REUSEADDR option
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # Bind and listen
+        s.bind((localhost_network, socket_port))
+        s.listen()
+
+        # Accept connections
+        while True:
+            (clientConnected, _) = s.accept()
+            try:
+                data_from_client = clientConnected.recv(1024)
+                data_to_print = data_from_client.decode()
+                print(data_to_print)
+            finally:
+                # Close the connection
+                clientConnected.close()
+    except socket.error:
+        pass
+    except KeyboardInterrupt:
+        pass
+    except Exception:
+        pass
+    finally:
+        s.close()
 
 
 def main_func():
     try:
-        while True:
-            current_time = time.strftime("%Y-%m-%d %H:%M:%S")
-            print("Current time:", current_time)
-            time.sleep(5)
+        parser = argparse.ArgumentParser(allow_abbrev=False)
+
+        # Define start argument
+        parser.add_argument(
+            '--start',
+            action='store_true',
+            help="Use this argument on systemd"
+        )
+
+        args = parser.parse_args()
+
+        # Condition when user pass argument start
+        if args.start:
+            while True:
+                send_data()
+                time.sleep(5)
+        else:
+            receive_data()
     except KeyboardInterrupt:
         pass
 
